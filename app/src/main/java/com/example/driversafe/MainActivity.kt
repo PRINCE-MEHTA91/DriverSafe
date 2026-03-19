786b4d85e32cf1b817f37aac248148d6cd8b811b
PRINCE-MEHTA91 <princekumar01zc@gmail.com>
2026-03-19 23:57:09 +0530

    done dashboard

--- app/src/main/java/com/example/driversafe/MainActivity.kt
+++ app/src/main/java/com/example/driversafe/MainActivity.kt
@@ -2,42 +2,37 @@ package com.example.driversafe
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
-    private lateinit var welcomeText: TextView
-    private lateinit var logoutBtn: Button
+    private lateinit var logoutBtn:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         mAuth = FirebaseAuth.getInstance()
 
-
+        // check user login
         if (mAuth.currentUser == null) {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
             return
         }
 
-
-        welcomeText = findViewById(R.id.welcomeText)
         logoutBtn = findViewById(R.id.logoutBtn)
 
-        val userEmail = mAuth.currentUser?.email
-        val user = mAuth.currentUser
-        val userName = user?.displayName
-        welcomeText.text = "Welcome $userName\n$userEmail"
-
         logoutBtn.setOnClickListener {
+
             mAuth.signOut()
+
             startActivity(Intent(this, LoginActivity::class.java))
+
             finish()
         }
     }
